3bac437df062bfd0b1e7cf430de68946b09b0cfd
Na1k <nick.kramer9926@gmail.com>
2022-02-03 11:25:21 +0100

    Changed path handling ("\\" --> r"/") Update gitignore for PyCharm generated folders

--- src/reader.py
+++ src/reader.py
@@ -4,16 +4,16 @@ import pandas as pd
 class Reader:
     df = pd.DataFrame()
 
-    def read_data(self, folders=43, pre_path=".\\data\\"):
+    def read_data(self, folders=43, pre_path=r"./data/"):
 
         for i in range(0, folders):
             if i < 10:
-                path = pre_path + str(i) + "\\GT-0000" + str(i) + ".csv"
+                path = pre_path + str(i) + r"/GT-0000" + str(i) + ".csv"
             else:
-                path = pre_path + str(i) + "\\GT-000" + str(i) + ".csv"
+                path = pre_path + str(i) + r"/GT-000" + str(i) + ".csv"
 
             df_tmp = pd.read_csv(path, sep=";")
-            df_tmp.insert(1, "Folder", pre_path + str(i) + "\\")
+            df_tmp.insert(1, "Folder", pre_path + str(i) + r"/")
             self.df = self.df.append(df_tmp)
 
         self.df.reset_index(inplace=True)
